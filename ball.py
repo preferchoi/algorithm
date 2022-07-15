0f8d44215f3daae61b1699e50ee2498521948b65
prefer_choi <prefer_choi@naver.com>
2022-07-15 15:12:15 +0900

    22/07/15

---
+++ ball.py
@@ -0,0 +1,17 @@
+for tc in range(1, 1+ int(input())):
+    mapp = list(input())
+    # print(mapp)
+    # while '|' in mapp:
+    #     mapp.remove('|')
+    print(mapp)
+
+    count = 0
+    ans = 0
+    before = ''
+    for i in mapp:
+        if i in [')', '|'] and before == '(':
+            ans += 1
+        elif before in ['(', '|'] and i == ')':
+            ans += 1
+        before = i
+    print(f'#{tc} {ans}')
