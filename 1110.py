1660fae04ef4e560b578af3c778e6851765b36a8
prefer_choi <prefer_choi@naver.com>
2022-08-03 23:54:01 +0900

    0803

--- 1110.py
+++ 1110.py
@@ -6,5 +6,20 @@
 
 '''
 N = int(input())
-while True:
-    
+before = N
+now = -1
+A, B = N // 10, N % 10
+conut = 0
+while conut < 5:
+    print(now, A, B)
+    conut += 1
+    A, B = now // 10, now % 10
+
+    if A:
+        now = A + B
+
+    else:
+        now = before % 10 * 10 + B
+
+    before = now
+
