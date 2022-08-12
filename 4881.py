544ee440e1114da3ba8ace43d995e4213d7ce1fc
prefer_choi <prefer_choi@naver.com>
2022-08-12 09:04:36 +0900

    0812

--- 4881.py
+++ 4881.py
@@ -1,45 +1,19 @@
-'''
-3
-3
-2 1 2
-5 8 5
-7 2 2
-3
-9 4 7
-8 6 5
-5 3 7
-5
-5 2 1 1 9
-3 3 8 3 1
-9 2 8 8 6
-1 5 7 8 3
-5 5 4 6 8
-
-'''
 from itertools import permutations
+
+
+def find():
+    mv = 100
+    for p in permutations(range(N)):
+        sv = 0
+        for i in range(N):
+            sv += aaaaaa[i][p[i]]
+        if mv > sv:
+            mv = sv
+    return mv
+
 
 for tc in range(1, 1 + int(input())):
     N = int(input())
-    N_list = [list(map(int, input().split())) for _ in range(N)]
+    aaaaaa = [list(map(int, input().split())) for _ in range(N)]
 
-    minV = 100
-
-    for p in permutations(range(N)):
-        sumV = 0
-        for i in range(N):
-            sumV += N_list[i][p[i]]
-
-        if minV > sumV:
-            minV = sumV
-
-    print(f'#{tc} {minV}')
-#
-
-
-
-
-
-
-
-
-
+    print('#{} {}'.format(tc, find()))
