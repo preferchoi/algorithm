efbb710ec07194c4545eacd5ca0018a648b27a71
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-08-27 21:04:42 +0900

    인덱스 바꾸기

---
+++ programmers/LV.0/인덱스 바꾸기.py
@@ -0,0 +1,8 @@
+def solution(my_string, num1, num2):
+    my_string = list(my_string)
+    my_string[num1], my_string[num2] = my_string[num2], my_string[num1]
+    
+    answer = ''
+    for i in my_string:
+        answer += i
+    return answer
