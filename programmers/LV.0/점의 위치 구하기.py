d3880d165ac80f77bc5cce91839ae31dec1bba9b
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-08-29 13:28:20 +0900

    점의 위치 구하기

---
+++ programmers/LV.0/점의 위치 구하기.py
@@ -0,0 +1,15 @@
+def solution(dot):
+    if dot[0] < 0 and dot[1] < 0:
+        answer = 3
+    
+    elif dot[0] < 0 and dot[1] > 0:
+        answer = 2
+    
+    elif dot[0] > 0 and dot[1] < 0:
+        answer = 4
+    
+    elif dot[0] > 0 and dot[1] > 0:
+        answer = 1
+    
+    
+    return answer
