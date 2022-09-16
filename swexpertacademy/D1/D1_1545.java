d71d4f6a97c7930abd0e604533f687efe5d626e2
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-09-16 18:27:38 +0900

    D1_1545

---
+++ swexpertacademy/D1/D1_1545.java
@@ -0,0 +1,18 @@
+package swexpertacademy.D1;
+
+import java.util.Scanner;
+
+public class D1_1545 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		for (int i = t; i >=0 ; i--) {
+			System.out.print(i+" ");
+		}
+	}
+
+}
