4b6cdd2a53cd1b1b44989629468cdb8eb42ccefa
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-09-19 09:18:56 +0900

    D1_1938

---
+++ swexpertacademy/D1/D1_1938.java
@@ -0,0 +1,23 @@
+package swexpertacademy.D1;
+
+import java.util.Scanner;
+
+public class D1_1938 {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+	
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			System.out.println(num1+num2);
+			System.out.println(num1-num2);
+			System.out.println(num1*num2);
+			System.out.println(num1/num2);
+			
+		
+	}
+
+}
