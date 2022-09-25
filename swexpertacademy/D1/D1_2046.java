4909346726e4110f8f654b098b9009f0b1c0b8d5
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-09-25 17:28:50 +0900

    D1_2046

---
+++ swexpertacademy/D1/D1_2046.java
@@ -0,0 +1,18 @@
+package swexpertacademy.D1;
+
+import java.util.Scanner;
+
+public class D1_2046 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		for (int i = 0; i < num; i++) {
+			System.out.print("#");
+		}
+	}
+
+}
