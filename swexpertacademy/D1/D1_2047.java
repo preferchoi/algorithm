b27ee97443f4e2103a58a7bbb2c2b4fcddd1a03d
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-09-26 16:35:00 +0900

    D1_2047

---
+++ swexpertacademy/D1/D1_2047.java
@@ -0,0 +1,17 @@
+package swexpertacademy.D1;
+
+import java.util.Scanner;
+
+public class D1_2047 {
+
+   public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+      
+      String in = sc.next();
+      
+      System.out.println(in.toUpperCase());
+      
+
+	}
+}
