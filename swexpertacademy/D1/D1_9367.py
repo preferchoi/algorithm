ccebe77f842e9572e08b70e64a564c6a6e30699b
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-10-05 08:48:51 +0900

    D1_9367

---
+++ swexpertacademy/D1/D1_9367.py
@@ -0,0 +1,16 @@
+for tc in range(1, 1 + int(input())):
+    N = int(input())
+    N_list = list(map(int, input().split()))
+
+    cnt = 0
+    pre = 0
+    maxV = 0
+    for i in N_list:
+        cnt += 1
+        if pre <= i:
+            if maxV < cnt:
+                maxV = cnt
+        else:
+            cnt = 1
+        pre = i
+    print(f'#{tc} {maxV}')
