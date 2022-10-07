0930f79ddeac01d20e7b1d264d0fffe6aaae91a6
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-10-07 19:32:47 +0900

    D2_1204

---
+++ swexpertacademy/D2/D2_1204.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for test in range(1, T+1):
+    num = int(input())
+    N_list = input().split()
+    N_list.sort()
+    # print(N_list)
+
+    count = 0
+    max = 0
+    for i in range(len(N_list)-2):
+        if N_list[i] != N_list[i+1]:
+            if max <= count:
+                max = count
+                max_num = N_list[i]
+            count = 0
+        count += 1
+    print(f'#{num}', max_num)
