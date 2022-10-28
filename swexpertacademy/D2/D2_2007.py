9f7aa27864556007e73b2461063fd230940b13eb
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-10-28 13:20:25 +0900

    D2_2007

---
+++ swexpertacademy/D2/D2_2007.py
@@ -0,0 +1,12 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    string = input()
+    for i in range(1, 11):
+        long = len(string[0:i])
+        f_string = string[0:i]
+        s_string = string[i:i+long]
+        if f_string == s_string:
+            # print("f_string:", f_string, "s_string:", s_string)
+            print("#{}".format(test_case), long)
+            break
