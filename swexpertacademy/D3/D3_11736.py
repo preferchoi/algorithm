e245522925e01e45c2ef4dddf2b2030b354dd303
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-11-08 19:03:49 +0900

    D3_11736

---
+++ swexpertacademy/D3/D3_11736.py
@@ -0,0 +1,6 @@
+for t in range(int(input())):
+    N, s, c = int(input()), list(map(int, input().split(" "))), 0
+    for i in range(1, N-1):
+        if s[i-1] < s[i] < s[i+1] or s[i+1] < s[i] < s[i-1]:
+            c += 1
+    print(f'#{t+1}', c)
