f7af16b0c77ebdb0004e7b85c609fb77eba344b3
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-12-02 14:25:10 +0900

    D3_3131

---
+++ swexpertacademy/D3/D3_3131.py
@@ -0,0 +1,28 @@
+n = 1000000
+sieve = [True] * n
+
+m = int(n ** 0.5)
+for i in range(2, m + 1):
+    if sieve[i] == True:
+        for j in range(i+i, n, i):
+            sieve[j] = False
+for x in [i for i in range(2, n) if sieve[i] == True]:
+    print(x, end=" ")
+# 이거 왜이리 어려움...
+# i = 10
+# sosoo = [2, 3, 5, 7]
+#
+# while 100 >= i:
+#     i += 1
+#     count = 0
+#
+#     for K in sosoo:
+#         if i % K == 0:
+#             break
+#         count += 1
+#         if count == len(sosoo):
+#             sosoo.append(i)
+#
+# for i in sosoo:
+#     print(i, end=" ")
+# 이르케풀면 시간초과되서 안댐
