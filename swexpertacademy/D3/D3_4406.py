3c34084e87675a723626d6a2f99ddf924d417e07
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-12-13 21:10:30 +0900

    D3_4406

---
+++ swexpertacademy/D3/D3_4406.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for test in range(1, T+1):
+    string = input()
+    print(f'#{test}', end=" ")
+    for i in string:
+        if i != "a" and i != 'e' and i != 'i' and i != 'o' and i != 'u':
+            print(i, end="")
+    print()
