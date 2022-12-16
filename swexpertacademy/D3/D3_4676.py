95f8d6222b427f4a7f9bc35a496d367c30f7c537
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-12-16 09:58:17 +0900

    D3_4676

---
+++ swexpertacademy/D3/D3_4676.py
@@ -0,0 +1,13 @@
+for test_case in range(1, int(input())+1):
+    word = list(input())
+
+    ti = int(input())
+    ma = list(map(int, input().split(" ")))
+    ma.sort()
+    ma.reverse()
+    for i in range(ti):
+        word.insert(ma[i], '-')
+    print(f'#{test_case}', end=' ')
+    for i in word:
+        print(i, end="")
+    print()
