616082b9488e6c3ba623808171bc2b0e7dc0de56
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-12-18 17:48:04 +0900

    D3_4751

---
+++ swexpertacademy/D3/D3_4751.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for test_case in range(1, T + 1):
+    case = input()
+    if len(case) != 0:
+        list = [['.'], ['.'], ['#'], ['.'], ['.']]
+        for i in case:
+            list[0].extend(['.', '#', '.', '.'])
+            list[1].extend(['#', '.', '#', '.'])
+            list[2].extend(['.', i, '.', '#'])
+            list[3].extend(['#', '.', '#', '.'])
+            list[4].extend(['.', '#', '.', '.'])
+
+    for i in list:
+        for j in range(len(i)):
+            print(i[j], end="")
+        print()
