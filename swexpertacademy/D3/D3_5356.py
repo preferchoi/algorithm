729d7e32852077e4884c3790b983d87f2c671b39
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-12-22 14:56:22 +0900

    D3_5356

---
+++ swexpertacademy/D3/D3_5356.py
@@ -0,0 +1,15 @@
+for test in range(1, int(input())+1):
+    array = []
+    for i in range(5):
+        array.append(list(map(str, input())))
+    Max = 0
+    for i in range(4):
+        if len(array[Max]) < len(array[i+1]):
+            Max = i+1
+    print(f'#{test}', end=" ")
+    for i in range(len(array[Max])):
+        for j in range(5):
+            if i >= len(array[j]):
+                continue
+            print(array[j][i], end="")
+    print()
