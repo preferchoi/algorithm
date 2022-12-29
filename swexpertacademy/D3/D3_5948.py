e9eb0df81dc5c717a885c54fbdf1c521fa7e9392
preferchoi <74041004+preferchoi@users.noreply.github.com>
2022-12-29 12:38:47 +0900

    D3_5948

---
+++ swexpertacademy/D3/D3_5948.py
@@ -0,0 +1,15 @@
+for test in range(1, int(input())+1):
+    num_list = list(map(int, input().split(" ")))
+    num_list.sort()
+    num_list.reverse()
+
+    sum_list = []
+    for i in num_list:
+        for j in num_list:
+            for k in num_list:
+                if not i == j and not i == k and not j == k:
+                    sum_list.append(i + j + k)
+    sum_list = list(set(sum_list))
+    sum_list.sort()
+    sum_list.reverse()
+    print(f'#{test}', sum_list[4])
